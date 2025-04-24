af4d1f11aee86ef5780fdb63488d0e6fdda815db
arjun s kumar <arjun@simplogics.com>
2025-04-24 15:15:01 +0530

    format routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,30 +4,30 @@ import { useAuth } from "../composables/useAuth";
 import { Locations } from "../constants/locations";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: Locations.BASE,
-    component: Layout, 
-    children: [
-      {
-        path: Locations.ADMIN_HOME,
-        name: "Dashboard",
-        component: () => import("../views/DashboardComponent.vue"), // Lazy-loaded Dashboard component
-        meta: { title: "Dashboard",requiresAuth: true },
-      },
-    ],
-  },
-  {
-    path: Locations.LOGIN,
-    name: "Login",
-    component: () => import("../views/Login.vue"), // Login page outside of the Layout
-    meta: { title: "Login" },
-  },
-  // {
-  //   path: "/:pathMatch(.*)*", // Catch-all route for 404 pages
-  //   name: "NotFound",
-  //   component: () => import("../views/NotFound.vue"),
-  //   meta: { title: "404 Not Found" },
-  // },
+	{
+		path: Locations.BASE,
+		component: Layout,
+		children: [
+			{
+				path: Locations.ADMIN_HOME,
+				name: "Dashboard",
+				component: () => import("../views/DashboardComponent.vue"), // Lazy-loaded Dashboard component
+				meta: { title: "Dashboard", requiresAuth: true },
+			},
+		],
+	},
+	{
+		path: Locations.LOGIN,
+		name: "Login",
+		component: () => import("../views/Login.vue"), // Login page outside of the Layout
+		meta: { title: "Login" },
+	},
+	// {
+	//   path: "/:pathMatch(.*)*", // Catch-all route for 404 pages
+	//   name: "NotFound",
+	//   component: () => import("../views/NotFound.vue"),
+	//   meta: { title: "404 Not Found" },
+	// },
 ];
 
 const router = createRouter({
@@ -35,16 +35,15 @@ const router = createRouter({
 	routes,
 });
 
-
-router.beforeEach((to, from, next) => {
-  const { isAuthenticated } = useAuth()
-  if (to.meta.requiresAuth && !isAuthenticated) {
-    next({ path: Locations.LOGIN })
-  } else if ((to.path === '/login' || to.path === '/forgot-password') && isAuthenticated) {
-    next({ path: Locations.BASE }) 
-  } else {
-    next()
-  }
+router.beforeEach((to, _, next) => {
+	const { isAuthenticated } = useAuth();
+	if (to.meta.requiresAuth && !isAuthenticated) {
+		next({ path: Locations.LOGIN });
+	} else if ((to.path === "/login" || to.path === "/forgot-password") && isAuthenticated) {
+		next({ path: Locations.BASE });
+	} else {
+		next();
+	}
 });
 
 export default router;
