3cd3d3a89d1418bbeb10c098e2f21dcf36100957
arjun s kumar <arjun@simplogics.com>
2025-04-24 18:16:37 +0530

    eslint.json

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import router from "./router";
 import "./style.css";
 import "./common.scss";
+import "element-plus/dist/index.css";
 
 const app = createApp(App);
 app.use(router);
