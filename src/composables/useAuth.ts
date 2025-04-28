e30b4881652a836f90bc9d4e1809d0fbc65a8e12
arjun s kumar <arjun@simplogics.com>
2025-04-28 17:31:17 +0530

    dynamic import routes

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -40,6 +40,7 @@
 
 export function useAuth() {
 	const token = localStorage.getItem("access_token");
+	const userRole = localStorage.getItem("access_token");
 	const isAuthenticated = !!token;
-	return { isAuthenticated };
+	return { isAuthenticated, userRole };
 }
